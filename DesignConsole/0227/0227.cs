67cf26f470f03d5bf280895830a7e902dfe6a8cd
yinweikang <yinweikang@c2matica.com>
2024-02-27 23:06:42 +0800

    0227实例

---
+++ DesignConsole/0227/0227.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignConsole._0227
+{
+    public class Cat
+    {
+        private string name;
+        public Cat(string name) {
+            this.name = name;
+        }
+
+        public delegate void CatShoutEventHandler();
+
+        public event CatShoutEventHandler CatShout;
+
+        public void Shout()
+        {
+            Console.WriteLine($"我是{name}");
+            if (CatShout != null)
+            {
+                CatShout();
+            }
+                
+        }
+    }
+
+    public class Mouse
+    {
+        private string name;
+        public Mouse(string name)
+        {
+            this.name = name;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"猫来了，{name}快跑");
+        }
+    }
+}
---
+++ DesignConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesignConsole._0227;
+
+Console.WriteLine("Hello, World!");
+Cat cat = new Cat("Tome");
+Mouse m1 = new Mouse("Jerry");
+Mouse m2 = new Mouse("Jack");
+cat.CatShout += new Cat.CatShoutEventHandler(m1.Run);
+cat.CatShout += new Cat.CatShoutEventHandler(m2.Run);
+cat.Shout();
+Console.ReadLine();
